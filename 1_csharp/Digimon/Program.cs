5b2bf563c807401fc3856b1652a0020260d2d223
Roby Thomas <roby.thomas1020@gmail.com>
2020-02-26 11:26:05 -0600

    add csharp directory

---
+++ 1_csharp/Digimon/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+namespace Digimon
+{
+  
+  internal class Digimon
+  {
+    
+  }
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      //var p = new Program();
+      Fight(null, null);
+    }
+
+    public static void Fight(Digimon a, Digimon b)
+    {
+      WriteLine("{0} fights {1}", a, b);
+    }
+  }    
+}
---
+++ 1_csharp/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using sc = System.Console;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string response = "";
+      while (response != "Hello World")
+      {
+        sc.Write("Enter 'Hello World': ");
+        response = sc.ReadLine();
+        sc.WriteLine("You wrote {0}", response);
+        if (response != "Hello World")
+        {
+          sc.WriteLine("Wrong input. Try again :(");
+        }
+      }
+      sc.WriteLine("Great Job!");
+    }
+  }
+}
